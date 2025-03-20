b7ba0569bf49df118b74d6f6afc773af3c8ec4dc
PatiphatRattanosot <654259017@webmail.npru.ac.th>
2025-03-20 16:25:23 +0700

    Enhance database connection logging and provide a default MongoDB URI

--- src/database/db.setup.ts
+++ src/database/db.setup.ts
@@ -1,10 +1,18 @@
 import mongoose from "mongoose";
 
 // Connect to the MongoDB database
-const mongodbURI = process.env.DB_URL;
+const mongodbURI =
+  process.env.DB_URL || "mongodb://localhost:27017/dev-visit-home";
 
 mongoose
   .connect(mongodbURI)
-  .then(() => console.log("Connected to the database"))
+  .then(() =>
+    console.log(
+      "Connected to the database : " +
+        mongoose.connection.host +
+        ":" +
+        mongoose.connection.port
+    )
+  )
   .catch((err) => console.log("Database connection error : " + err));
 export default mongoose;
